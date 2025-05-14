77ba8eebd4afa23ec05481b5001d9b08fd9e6b59
Jhoanis_rq <Jhoanisrq@gmail.com>
2025-05-13 23:12:32 -0500

    Add files via upload

--- Dulcelatto/pages/productos.php
+++ Dulcelatto/pages/productos.php
@@ -5,13 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Productos - Dulce Latte</title>
     <link rel="stylesheet" href="../assets/css/styles.css">
+    <link rel="stylesheet" href="../assets/css/productos.css">
     <link rel="website icon" type="png" href="../assets/img/logo.png">
 </head>
 <body>
     <?php include '../includes/header.php'; ?>
 
     <main>
-        <h1>Nuestros Postres</h1>
+        <h1>Nuestro Catalogo</h1>
         <section class="productos-grid">
             <!-- Los productos se cargarán dinámicamente con JS -->
         </section>
